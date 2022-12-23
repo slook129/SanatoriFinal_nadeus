8a46cc60ecdcc45a68cc1024b200b14881450c56
EgorNa <egrghost15@gmail.com>
2022-12-23 20:19:10 +0400

    Добавлены начальные классы гостя, услуги и пользователя

---
+++ sanatoriy/sanatoriy/Guest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sanatoriy
+{
+    internal class Guest : IComparable
+    {
+        private string role; //роль человека в система
+        private string medpolic; //медицинский полис человека
+        public string Role { get { return role; } set { role = value; } }
+        public string Medpolic { get { return medpolic; } set { medpolic = value; } }
+
+        public int CompareTo(object? obj)
+        {
+            Guest comparing = obj as Guest;
+            return comparing.Medpolic.CompareTo(this.Medpolic);
+        }
+        public Guest()
+        {
+            role = "guest";
+            medpolic = "";
+        }
+    }
+}
---
+++ sanatoriy/sanatoriy/Service.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sanatoriy
+{
+    internal class Service
+    {
+        string schedule;
+        string price;
+        string description;
+        public string Schedule { get { return schedule; } set { Schedule = value; } }
+        public string Price { get { return price; } set { price = value; } }
+        public string Desription { get { return description; } set { description = value; } }
+        /// <summary>
+        /// //Конструктор класса услуга, который создает элементы класса Услуга.
+        /// </summary>
+        public Service()
+        {
+            schedule = "";
+            price = "";
+            description = "";
+        }
+    }
+}
